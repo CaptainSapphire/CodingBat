98aa1b22dbea78d873e67779d818d539ea0cc49c
misty_stars <122335357+CaptainSapphire@users.noreply.github.com>
2025-10-27 10:15:10 -0700

    Create main.py

---
+++ Python/Warmup-1/near_100/main.py
@@ -0,0 +1,10 @@
+def near_hundred(n):
+    # check for 100
+  if (n > 89 and n < 111):
+    return True;
+  # check for 200
+  if (n > 189 and n < 211):
+    return True;
+  else:
+    return False;
+    
